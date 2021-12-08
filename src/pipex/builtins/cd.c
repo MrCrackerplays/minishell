6c7d2fbd9dc2b7ae4193b3ab60af284c31f5ad90
Robin Drazsky <rdrazsky@f0r3s19.codam.nl>
2021-12-08 15:29:07 +0100

    not ft fix

--- src/pipex/builtins/cd.c
+++ src/pipex/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: rdrazsky <rdrazsky@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/08 13:36:37 by rdrazsky      #+#    #+#                 */
-/*   Updated: 2021/12/08 15:20:50 by rdrazsky      ########   odam.nl         */
+/*   Updated: 2021/12/08 15:25:29 by rdrazsky      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ bool	pipex_cd(t_strlist *lst)
 {
 	if (!lst)
 		return (true);
-	if (strncmp(lst->str->text, "~", 2) == 0)
+	if (ft_strncmp(lst->str->text, "~", 2) == 0)
 		chdir(getenv("HOME"));
 	else
 		chdir(lst->str->text);
