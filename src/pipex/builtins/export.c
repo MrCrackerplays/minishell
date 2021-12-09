371a74abb3148df0b955b4c978242cc7acd00660
Robin Drazsky <rdrazsky@f0r3s24.codam.nl>
2021-12-09 13:40:08 +0100

    env get function and minor env fix

--- src/pipex/builtins/export.c
+++ src/pipex/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: rdrazsky <rdrazsky@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/08 19:07:27 by rdrazsky      #+#    #+#                 */
-/*   Updated: 2021/12/08 19:16:19 by rdrazsky      ########   odam.nl         */
+/*   Updated: 2021/12/09 13:31:11 by rdrazsky      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,19 @@ static void	remove_from_env(t_string *str)
 	t_strlist	*og_lst;
 	t_strlist	*lst;
 	int			len;
+	t_vars		*vars;
 
 	len = 0;
 	while (str->text[len] != '=')
 		len++;
-	og_lst = get_t_vars()->env;
+	vars = get_t_vars();
+	og_lst = vars->env;
 	lst = og_lst;
 	while (lst)
 	{
 		if (ft_strncmp(lst->str->text, str->text, len) == 0)
 		{
-			ft_strlst_remove_free(&og_lst, lst);
+			ft_strlst_remove_free(&vars->env, lst);
 			return ;
 		}
 		lst = lst->next;
@@ -36,9 +38,23 @@ static void	remove_from_env(t_string *str)
 
 void	pipex_export(t_strlist *lst)
 {
+	int			len;
+
 	if (!lst)
 		return ;
-	if (ft_strchr(lst->str->text, '='))
+	len = 0;
+	while (lst->str->text[len] != '=')
+	{
+		if (!ft_isalpha(lst->str->text[len])
+			&& !ft_isdigit(lst->str->text[len])
+			&& lst->str->text[len] != '_')
+		{
+			len = -1;
+			break ;
+		}
+		len++;
+	}
+	if (len != -1 && ft_strchr(lst->str->text, '='))
 	{
 		remove_from_env(lst->str);
 		ft_strlst_add_back(&get_t_vars()->env,
--- src/pipex/utils/vars.c
+++ src/pipex/utils/vars.c
@@ -6,7 +6,7 @@
 /*   By: rdrazsky <rdrazsky@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/08 17:23:17 by rdrazsky      #+#    #+#                 */
-/*   Updated: 2021/12/08 17:34:19 by rdrazsky      ########   odam.nl         */
+/*   Updated: 2021/12/09 13:34:09 by rdrazsky      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,23 @@ void	t_vats_init(char **envp)
 		envp++;
 	}
 	vars->vars = NULL;
+	vars->last_out = ft_str_new("0");
+}
+
+char	*get_env_var(char *name)
+{
+	t_strlist	*lst;
+	char		*tmp;
+
+	if (ft_strncmp(name, "?", 2) == 0)
+		return (get_t_vars()->last_out->text);
+	lst = get_t_vars()->env;
+	while (lst)
+	{
+		tmp = ft_strchr(lst->str->text, '=');
+		if (ft_strncmp(lst->str->text, name, tmp - lst->str->text) == 0)
+			return (tmp + 1);
+		lst = lst->next;
+	}
+	return (NULL);
 }
